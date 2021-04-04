ba22f212696407f9882fe4de56974afa591d253c
PiratePanda978 <kunwar9789@gmail.com>
2021-04-04 13:29:15 +0530

    Modified

--- week7/Packages/question1/MATRIX.java
+++ week7/Packages/question1/MATRIX.java
@@ -1,45 +1,52 @@
 package Anupam;
+
 import java.util.Scanner;
-public class MATRIX{
+
+public class MATRIX {
     public int[][] arr = new int[3][3];
-    int i,j;
-    public MATRIX(){
-        for(i=0;i<3;i++){
-            for(j=0;j<3;j++){
+    int i, j;
+
+    public MATRIX() {
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
                 arr[i][j] = 0;
             }
         }
     }
-    public void readArr(){
+
+    public void readArr() {
         Scanner in = new Scanner(System.in);
-        for(i=0;i<3;i++){
-            for(j=0;j<3;j++){
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
                 arr[i][j] = in.nextInt();
             }
         }
     }
-    public void printArr(){
-        for(i=0;i<3;i++){
-            for(j=0;j<3;j++){
-                System.out.print(arr[i][j]+" ");
+
+    public void printArr() {
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
-    public int[][] addArray(int[][] arr2){
+
+    public int[][] addArray(int[][] arr2) {
         int[][] sum = new int[3][3];
-        for(i=0;i<3;i++){
-            for(j=0;j<3;j++){
-                sum[i][j] = arr[i][j]+arr2[i][j];
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                sum[i][j] = arr[i][j] + arr2[i][j];
             }
         }
         return sum;
     }
-    public int[][] subArray(int[][] arr2){
+
+    public int[][] subArray(int[][] arr2) {
         int[][] diff = new int[3][3];
-        for(i=0;i<3;i++){
-            for(j=0;j<3;j++){
-                diff[i][j] = arr[i][j]-arr2[i][j];
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                diff[i][j] = arr[i][j] - arr2[i][j];
             }
         }
         return diff;
