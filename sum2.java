5b3031374bbff947d02cd6fc306bbda2def3aeb8
PiratePanda978 <kunwar9789@gmail.com>
2021-05-08 21:11:20 +0530

    m

--- sum2.java
+++ sum2.java
@@ -1,6 +1,6 @@
 public class sum2{
     public static void main(String args[]){
-        int[] arr = {1,3,2};
+        int[] arr = {2,3,2};
         int sum=0;
         for(int i=0;i<arr.length;i++)
         sum+=arr[i];
